66a89da291231e8bc79c735be6a4f091ab01b425
Adrian Nita <adrinium@gmail.com>
2024-03-17 15:36:59 +0200

    Added @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./card/card.component";
+import { Card } from './models/card';
+import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CardComponent]
+    imports: [RouterOutlet, CardComponent, NgFor]
 })
 export class AppComponent {
   title = 'cards';
+
+  cards: Card[] = [
+    new Card("Neat tree","#nature","Saw this awsome tree during my hike today.","assets/images/tree.jpeg"),
+    new Card("Snowy mountain","#hikingperson","Beautiful view of some mountains I saw during my hike.","assets/images/mountain.jpeg"),
+    new Card("Mountain biking","#hiking666","Action shot of me riding my bike.","assets/images/biking.jpeg")
+  ];
 }
